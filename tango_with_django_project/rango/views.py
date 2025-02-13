e312a7aefba0bdda5538505431f2e2a19ef074fd
“Martin <2888769g@student.gla.ac.uk>
2025-02-13 03:00:46 +0000

    chapter 8 done

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -45,7 +45,7 @@ def add_category(request):
 
         if form.is_valid():
             form.save(commit=True)
-            return redirect('/rango/')
+            return redirect(reverse('rango:index'))
         else:
             print(form.errors)
     
@@ -56,9 +56,9 @@ def add_page(request, category_name_slug):
         category = Category.objects.get(slug=category_name_slug)
     except:
         category = None
-
+    
     if category is None:
-        return redirect('/rango/')
+        return redirect(reverse('rango:index'))
 
     form = PageForm()
 
@@ -74,7 +74,7 @@ def add_page(request, category_name_slug):
 
                 return redirect(reverse('rango:show_category', kwargs={'category_name_slug': category_name_slug}))
         else:
-            print(form.errors) 
+            print(form.errors)  
     
     context_dict = {'form': form, 'category': category}
     return render(request, 'rango/add_page.html', context=context_dict)
