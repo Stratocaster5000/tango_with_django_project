6fb6835a172cef278ce4f9efa3af4afbe13db7b3
“Martin <2888769g@student.gla.ac.uk>
2025-02-13 02:46:12 +0000

    chapter 7 complete

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -22,7 +22,7 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'sq_z*m=yr)aiu$9lyzf@)&-!o(yjs=)9igwhuco$d7@a3jed=@'
+SECRET_KEY = '%etq%0r+5mg%u4^eaezh8rm=8(%doq(b^j#xkqmwr!wtj#nhwu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
